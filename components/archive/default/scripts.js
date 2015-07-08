633281628485849fb23d8686b5f68b086e4555fa
Dan Calinescu <hi@dancali.io>
2015-07-08 13:26:55 +0300

    Added data assets

--- components/archive/default/scripts.js
+++ components/archive/default/scripts.js
@@ -1,29 +1,13 @@
 module.exports = function ($, app)  {
 
-  return function($scope) {
+  return function($scope, $http) {
 
-    $scope.articles = [
-                       {
-                           link: "http://localhost:9001/",
-                           image: "https://download.unsplash.com/photo-1434139240289-56c519f77cb0",
-                           title: "The Title",
-                           date: "7-May-2015",
-                           body: "The Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore, sed do eiusmod tempor incididunt ut labore et dolore"
-                       },
-                       {
-                           link: "http://localhost:9001/",
-                           image: "https://download.unsplash.com/photo-1433959352364-9314c5b6eb0b",
-                           title: "The Title",
-                           date: "7-May-2015",
-                           body: "The Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore, sed do eiusmod tempor incididunt ut labore et dolore"
-                       },
-                       {
-                           link: "http://localhost:9001/",
-                           image: "https://download.unsplash.com/photo-1433838552652-f9a46b332c40",
-                           title: "The Title",
-                           date: "7-May-2015",
-                           body: "The Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore, sed do eiusmod tempor incididunt ut labore et dolore"
-                       }
-                   ];
+    $http.get('/data/articles.json').
+      success(function(data, status, headers, config) {
+        $scope.articles = data;
+      }).
+      error(function(data, status, headers, config) {
+
+    });
   }
 }
