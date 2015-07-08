754a155d9031940672860e8d741c6471d0f08d8e
Dan Calinescu <hi@dancali.io>
2015-07-08 18:38:05 +0300

    added archive

--- components/archive/default/scripts.js
+++ components/archive/default/scripts.js
@@ -1,13 +1,58 @@
 module.exports = function ($, app)  {
 
   return function($scope, $http) {
+    var allArticles         = [];
+
+    $scope.articles         = [];
+    $scope.perPage          =  3;
+    $scope.page             =  1;
+    $scope.totalArticles    =  0;
+    $scope.totalPages       =  0;
+    $scope.showArticles     = false;
+
+    function update () {
+      var start = ($scope.page-1) * $scope.perPage;
+      var end = ($scope.page-1) * $scope.perPage + $scope.perPage;
+      $scope.articles = allArticles.slice(start, end);
+    }
+
+    $scope.fetchOlder = function() {
+      if (!$scope.hasOlder()) {
+        return;
+      }
+
+      $scope.page = $scope.page + 1;
+      update();
+    }
+
+    $scope.fetchNewer = function () {
+      if (!$scope.hasNewer()) {
+        return;
+      }
+
+      $scope.page = $scope.page - 1;
+      update();
+    }
+
+    $scope.hasOlder = function() {
+      return ($scope.page < $scope.totalPages);
+    }
+
+    $scope.hasNewer = function() {
+      return ($scope.page > 1);
+    }
 
     $http.get('/data/articles.json').
       success(function(data, status, headers, config) {
-        $scope.articles = data;
+        allArticles           = data;
+        $scope.page           = 1;
+        $scope.totalArticles  = allArticles.length;
+        $scope.showArticles   = true;
+        $scope.totalPages     = Math.ceil($scope.totalArticles / $scope.perPage);
+
+        update();
       }).
       error(function(data, status, headers, config) {
-
-    });
-  }
+      });
+   }
 }
