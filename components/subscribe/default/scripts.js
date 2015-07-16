3105373a09533864cfad208a593bf95156b61671
rMiroslav <emanuel@lifenation.org>
2015-07-16 13:58:29 +0300

    fixed subscribe

--- components/subscribe/default/scripts.js
+++ components/subscribe/default/scripts.js
@@ -1,53 +1,49 @@
 module.exports = function ($, app, localeId)  {
 
   return function($scope, $http, $location, $sce, $timeout) {
+      $scope.danger  = false;
+      $scope.success = false;
+      $scope.name    = "";
+      $scope.email   = "";
 
-  	$scope.name          = "";
-  	$scope.email         = "";
-	  $scope.warning       = false;
-  	$scope.success       = false;
-    $scope.emailDanger   = false;
+      function clearAlert () {
+         $scope.danger  = false;
+         $scope.success = false;
+         addAttr(); 
+      }
 
-  	function cleanAlert() {
-  		$scope.warning     = false;
-  		$scope.success     = false;
-      $scope.emailDanger = false;
-  	}
-
-		
-
-  	$scope.subscribe = function(){
+      function clearModel () {
+        $scope.name    = "";
+        $scope.email   = "";
+      }
+       
+      var nameAttr    = angular.element( document.querySelector( '#subName' ));
+      var emailAttr   = angular.element( document.querySelector( '#subEmail' ));
       
-      $timeout(cleanAlert, 3000);
-  		 
-      if(!$scope.name && !$scope.email || $scope.name == "" || $scope.email  == "" || $scope.email == undefined){
-   			
-        $scope.warning     = true;
-        $scope.emailDanger = true;
-        $scope.emailAlert  = 'Please include "@" in email fild';
-  		
-      }else {
-  			$scope.success  = true;
-        $scope.name     = "";
-        $scope.email    = "";
-        
-  		}
-      
+      function removeAttrib () {
+        nameAttr.removeAttr('required');
+        emailAttr.removeAttr('required');
+      }
 
+      function addAttr () {
+        nameAttr.attr(   'required', "required");
+        emailAttr.attr(  'required', "required");
+      }
 
-    		// $http.post('/someUrl', {name:$scope.name, email:$scope.email}).
-      // success(function(data, status, headers, config) {
-      //   // this callback will be called asynchronously
-      //   // when the response is available
-      //   console.log(data);
-      // }).
-      // error(function(data, status, headers, config) {
-      //   // called asynchronously if an error occurs
-      //   // or server returns response with an error status.
-      //   console.log(status);
-      // });
-  		
-  	}
-			
-   }
+      $scope.send = function () {
+
+        $timeout(clearAlert, 3000);
+       if(!$scope.name && !$scope.email || $scope.name == "" || $scope.email == "") {
+          $scope.danger = true;
+       }
+
+        if($scope.danger) {
+          return;
+        }
+
+        $scope.success = true;
+        removeAttrib();
+        clearModel();
+      }  
+  }
 }
