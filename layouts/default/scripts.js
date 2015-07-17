9e2b9b3cbfbc5e43ed9a0625d5a52243433f7239
Dan Calinescu <hi@dancali.io>
2015-07-17 15:35:05 +0300

    fixed dynamic button text size

--- layouts/default/scripts.js
+++ layouts/default/scripts.js
@@ -1,9 +1,29 @@
 'use strict';
 
-	$.each($('.btn-action'), function(i, button){
-			var text = $(button).text();
-			$(button).text(text.substring(0,15));
-		});
+$.fn.textWidth = function(text, font) {
+    if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
+    $.fn.textWidth.fakeEl.text(text || this.val() || this.text()).css('font', font || this.css('font'));
+    return $.fn.textWidth.fakeEl.width();
+};
+
+$.fn.buttonTextWidth = function(text, button) {
+	return $.fn.textWidth(text,  $(button).css('font-size') + " " + $(button).css('font-family'));
+};
+
+// Common scripts
+$.each($('.btn-action'), function(i, button){
+		var text        = $(button).text();
+		var buttonWidth = $(button).innerWidth();
+		var textWidth   = $.fn.buttonTextWidth(text, button);
+		while (textWidth + 30 > buttonWidth) {
+			var fontSize = $(button).css('font-size');
+			fontSize = fontSize.substring(0, fontSize.length - 2);
+			fontSize = fontSize - 1;
+			fontSize = fontSize + "px";
+			$(button).css({'font-size': fontSize});
+			textWidth   = $.fn.buttonTextWidth(text, button);
+		}
+});
 
 var app = angular.module('carmel', ['ngSanitize', 'ngRoute']);
 
