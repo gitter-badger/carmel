58a3fe006dd1ff8fc53080fbb2819ef1e9d78ab9
rMiroslav <emanuel@lifenation.org>
2015-07-17 21:54:25 +0300

    prevent default

--- components/carousel/default/scripts.js
+++ components/carousel/default/scripts.js
@@ -3,6 +3,9 @@
 $('.carousel').carousel({
    interval: 5000
   });
+	$('.carousel-control').click(function(e){
+		e.preventDefault();
+	});
 
   return function($scope, $http, $location, $sce, $timeout) {
 
