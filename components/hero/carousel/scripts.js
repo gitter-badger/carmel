a5977e88c43bdb8c104279175bc6610f9577de55
rMiroslav <emanuel@lifenation.org>
2015-08-07 10:32:24 +0300

    added carousel hero

---
+++ components/hero/carousel/scripts.js
@@ -0,0 +1,10 @@
+module.exports = function ($, app, localeId, component)  {
+
+  $('.carousel').carousel({
+    interval: 2000
+  })
+
+  return function($scope, $http, $location, $sce, $timeout) {
+
+  }
+}
