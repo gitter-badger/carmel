9ac9f21e772bab06ba1563e14aa5379df14173e5
Dan Calinescu <hi@dancali.io>
2015-08-14 15:28:58 +0300

    fixed articles

--- components/article/default/scripts.js
+++ components/article/default/scripts.js
@@ -1,41 +1,6 @@
 module.exports = function ($, app, localeId)  {
 
-
   return function($scope, $http, $location, $sce, $timeout) {
+  }
 
-    var urlPath       = window.location.pathname;
-    var urlParts      = urlPath.split('/');
-    var partsExpected = (localeId ? 4 : 3);
-
-    if (!urlParts || urlParts.length < partsExpected || !urlParts[partsExpected - 1]){
-      return;
-    }
-
-    var idAndSlug   = urlParts[partsExpected-1].split('-');
-
-    if (!idAndSlug || idAndSlug.length < 2 || !idAndSlug[0] || !idAndSlug[1]) {
-      return;
-    }
-
-    var articleId   = idAndSlug[0];
-    var articleSlug = idAndSlug[1];
-
-    var dataUrl = '/' + (localeId ? localeId  + '/': '') + 'data/articles/' +  articleId;
-
-    console.log(dataUrl);
-
-    $http.get(dataUrl + '.html').
-      success(function(data, status, headers, config) {
-        $scope.article.body = $sce.trustAsHtml(data);
-      }).
-      error(function(data, status, headers, config) {
-      });
-
-  $http.get(dataUrl + '.json').
-     success(function(data, status, headers, config) {
-       $scope.article.meta = data;
-     }).
-     error(function(data, status, headers, config) {
-     });
-   }
 }
